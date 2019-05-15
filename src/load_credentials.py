3767a83a2fa5914aa142b7461081771146fbd261
Alex Koufakis <alkouf@hotmail.com>
2019-05-15 17:14:35 +0300

    if not able to load env vars then try to load the credentials from local file
    For local use.

--- src/load_credentials.py
+++ src/load_credentials.py
@@ -3,6 +3,12 @@ import json
 
 
 class LoadCredentials:
+    """
+    Loads the webgenesis credentials and bus credentials from environment variables.
+
+    If environment vars are not found then attempt to load from local files.
+    """
+
     # filename of wg entrypoint
     _wg_entrypoint_file = "webgenesis_entrypoint.json"
 
@@ -22,8 +28,18 @@ class LoadCredentials:
         :return: dictionary with keys ['api_key', 'kafka_admin_url', 'kafka_brokers_sasl']
         """
         cred = dict()
-        cred['api_key'] = os.environ[LoadCredentials._api_key_env]
-        cred['kafka_brokers_sasl'] = os.environ[LoadCredentials._kafka_brokers_sasl_env]
+
+        try:
+            cred['api_key'] = os.environ[LoadCredentials._api_key_env]
+            cred['kafka_brokers_sasl'] = os.environ[LoadCredentials._kafka_brokers_sasl_env]
+            print("kafka brokers sasl:" + os.environ[LoadCredentials._kafka_brokers_sasl_env])
+        except:
+            print(
+                "Error @ load bus credentials using the keys:" + LoadCredentials._api_key_env + " " + LoadCredentials._kafka_brokers_sasl_env)
+            bus_cred_file = "bus_credentials.json"
+            print("Trying to find values in file instead:" + bus_cred_file)
+            with open(bus_cred_file) as f:
+                return json.load(f)
 
         return cred
 
@@ -37,8 +53,19 @@ class LoadCredentials:
         """
 
         cred = dict()
-        cred['username'] = os.environ[LoadCredentials._wg_username_env]
-        cred['password'] = os.environ[LoadCredentials._wg_pass_env]
+        try:
+            cred['username'] = os.environ[LoadCredentials._wg_username_env]
+            cred['password'] = os.environ[LoadCredentials._wg_pass_env]
+        except:
+            print(
+                "Error @ load bus credentials using the keys:" + LoadCredentials._wg_username_env + " " + LoadCredentials._wg_pass_env)
+
+            # for the local deployment
+            wg_cred_path = "webgenesis_credentials.json"
+            print("Trying to find values in file instead:" + wg_cred_path)
+            with open(wg_cred_path, 'r') as f:
+                return json.load(f)
+
         with open(LoadCredentials._wg_entrypoint_file, "r") as f:
             wg = json.load(f)
         cred['hostname'] = wg['hostname']
@@ -48,6 +75,9 @@ class LoadCredentials:
 
 
 if __name__ == "__main__":
+    # LoadCredentials.load_bus_credentials()
+    # print("hey")
     os.environ["WG_PASSWORD"] = "XXX_pass"
     os.environ["WG_USERNAME"] = "XXX_username"
     print(LoadCredentials.load_wg_credentials())
+    # print(LoadCredentials.load_bus_credentials())
