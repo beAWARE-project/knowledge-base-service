5ae450434a29ec1e5028eaf73458872af604a96c
Panos Mitzias <panmitz@users.noreply.github.com>
2019-05-15 15:31:20 +0300

    removed function handle_topic()

--- src/bus_producer.py
+++ src/bus_producer.py
@@ -29,10 +29,6 @@ class BusProducer:
 
     def send(self, topic, message):
 
-        # Check if topic exists and create it if not
-        if not self.handle_topic(topic):
-            return False
-
         # Produce and flush message to bus
         try:
             self.producer.produce(topic, message.encode('utf-8'), 'key', -1, self.on_delivery)
@@ -41,43 +37,6 @@ class BusProducer:
             print('Sending data failed')
             print(err)
             return False
-
-        return True
-
-    def handle_topic(self, topic_name):
-
-        # Create rest client to handle topics
-        try:
-            rest_client = rest.MessageHubRest(self.credentials['kafka_admin_url'], self.credentials['api_key'])
-        except Exception as e:
-            print(e)
-            return False
-
-        # List all topics
-        try:
-            response = rest_client.list_topics()
-            topics = json.loads(response.text)
-        except Exception as e:
-            print(e)
-            return False
-
-        # Check if desired topic exists in topic list
-        topic_exists = False
-
-        for topic in topics:
-            if topic['name'] == topic_name:
-                topic_exists = True
-
-        # If topic does not exist
-        if not topic_exists:
-
-            # Create topic
-            try:
-                response = rest_client.create_topic(topic_name, 1, 1)
-                print(response.text)
-            except Exception as e:
-                print(e)
-                return False
 
         return True
 
